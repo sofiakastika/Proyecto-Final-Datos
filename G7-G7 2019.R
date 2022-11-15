bf063118f627cb698495dee517a841a4116a1e4f
sofiakastika <118311021+sofiakastika@users.noreply.github.com>
2022-11-15 18:08:36 -0300

    Update G7-G7 2019.R

--- G7-G7 2019.R
+++ G7-G7 2019.R
@@ -1,16 +1,20 @@
 #g7-g7 2019 
+
+#Importamos dataset con Expos e Impos de g7 2019
 library(readxl)
 g7_g7_2019 <- read_excel("econ-708/proyectos/g7-g7 2019.xlsx")
 View(g7_g7_2019)
-
+#Importamos dataset de nodos
 Nodos_Mundo <- read_excel("econ-708/proyectos/Nodos Mundo.xlsx")
 View(Nodos_Mundo)
 
+#Instalamos paquetes para redes
 install.packages("igraph") 
 install.packages("network") 
-
 library(igraph)
 library(network)
+
+#Realizamos la red 
 net <- graph_from_data_frame(d=g7_g7_2019, vertices=Nodos_Mundo, directed=T) 
 net
 plot(net)
