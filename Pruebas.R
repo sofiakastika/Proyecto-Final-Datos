e9c1696f15251cd75f10613a35dabff04ffaf7c0
MarckDG <56935024+MarckDG@users.noreply.github.com>
2022-12-01 16:02:46 -0300

    Update Pruebas.R

--- Pruebas.R
+++ Pruebas.R
@@ -59,6 +59,110 @@ ggplot(merc_net_2000,
 
 
 
+mercosur <- rbind (arg, br, uy, par, ven)          
+
+#colnames(mercosur)[5] <-  "Value"
+
+mercosur <- mercosur %>% relocate(Year, .after = last_col())                    #Pasa la columna año al ultimo lugar
+
+nodos_merc <- mercosur %>% select(Reporter)                                     #Creamos los nodos mercosur
+nodos_merc <- nodos_merc [!duplicated(nodos_merc), ]
+
+nodos_g7 <- mercosur %>% select(Partner)                                        #Creamos los nodos G7
+nodos_g7 <- nodos_g7 [!duplicated(nodos_g7), ]
+colnames(nodos_g7)[1] <-  "Reporter"
+
+                                            #Creamos los nodos de toda la red
+
+
+#Valor total de las exportaciones 
+
+nodos_merc_2000 <- mutate(nodos_merc, value = 0)
+nodos_merc_2010 <- mutate(nodos_merc, value = 0)
+nodos_merc_2019 <- mutate(nodos_merc, value = 0)
+
+for (i in 1:5) {
+  
+  nodos_merc_2000$value[i] <- sum(mercosur[mercosur$Reporter == nodos_merc_2000$Reporter[i] & mercosur$Year == 2000, ]$`Trade Value (US$)`)  
+}
+
+
+for (i in 1:5) {
+  
+  nodos_merc_2010$value[i] <- sum(mercosur[mercosur$Reporter == nodos_merc_2010$Reporter[i] & mercosur$Year == 2010, ]$`Trade Value (US$)`)  
+}
+
+for (i in 1:5) {
+  
+  nodos_merc_2019$value[i] <- sum(mercosur[mercosur$Reporter == nodos_merc_2019$Reporter[i] & mercosur$Year == 2019, ]$`Trade Value (US$)`)  
+}
+
+
+nodos_g7_2000 <- mutate(nodos_g7, value = 0)
+nodos_g7_2010 <- mutate(nodos_g7, value = 0)
+nodos_g7_2019 <- mutate(nodos_g7, value = 0)
+
+
+for (i in 1:7) {
+  
+  nodos_g7_2000$value[i] <- sum(mercosur[mercosur$Partner == nodos_g7_2000$Reporter[i] & mercosur$Year == 2000, ]$`Trade Value (US$)`)  
+}
+
+for (i in 1:7) {
+  
+  nodos_g7_2010$value[i] <- sum(mercosur[mercosur$Partner == nodos_g7_2010$Reporter[i] & mercosur$Year == 2010, ]$`Trade Value (US$)`)  
+}
+
+for (i in 1:7) {
+  
+  nodos_g7_2019$value[i] <- sum(mercosur[mercosur$Partner == nodos_g7_2019$Reporter[i] & mercosur$Year == 2019, ]$`Trade Value (US$)`)  
+}
+
+nodos_2000 <- rbind(nodos_merc_2000, nodos_g7_2000)
+nodos_2010 <- rbind(nodos_merc_2010, nodos_g7_2010)
+nodos_2019 <- rbind( nodos_merc_2019, nodos_g7_2019)
+
+#Creamos las redes
+merc_g7_net_2000 <- graph_from_data_frame(d= filter (mercosur,                
+                                                     Year == "2000" & `Trade Value (US$)` >= 1000000),
+                                          vertices= nodos_2000, directed=T)
+
+merc_g7_net_2000 <- ggnetwork(merc_g7_net_2000)                                 
+
+
+merc_g7_net_2010 <- graph_from_data_frame(d= filter (mercosur,                # Mercosur y G7 año 2010
+                                                     Year == "2010" & `Trade Value (US$)` >= 1000000),
+                                          vertices= nodos_2010, directed=T)
+
+merc_g7_net_2010 <- ggnetwork(merc_g7_net_2010) 
+
+merc_g7_net_2019 <-  graph_from_data_frame(d= filter (mercosur,                # Mercosur y G7 año 2019
+                                                      Year == "2019" & `Trade Value (US$)` >= 1000000),
+                                           vertices= nodos_2019, directed=T)
+
+merc_g7_net_2019 <- ggnetwork(merc_g7_net_2019)
+
+
+# Codigos de paises
+merc_g7_net_2000$country<-countrycode(merc_g7_net_2000$name,                    # Agregamos los codigos de paises para la visaulización con ggflags
+                                      "country.name","iso2c")
+merc_g7_net_2000$country <- merc_g7_net_2000$country %>% tolower()
+
+
+merc_g7_net_2010$country<-countrycode(merc_g7_net_2010$name,                    
+                                      "country.name","iso2c")
+merc_g7_net_2010$country <- merc_g7_net_2010$country %>% tolower()
+
+merc_g7_net_2019$country<-countrycode(merc_g7_net_2019$name,                    
+                                      "country.name","iso2c")
+merc_g7_net_2019$country <- merc_g7_net_2019$country %>% tolower()
+
+
+
+
+
+
+
 
 
 
