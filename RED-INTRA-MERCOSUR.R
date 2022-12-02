1f55dab973f2fbb8e398cb5aa4ec8dba3e03db02
MarckDG <56935024+MarckDG@users.noreply.github.com>
2022-12-02 07:55:19 -0300

    Update RED-INTRA-MERCOSUR.R

--- RED-INTRA-MERCOSUR.R
+++ RED-INTRA-MERCOSUR.R
@@ -1,5 +1,5 @@
 #Intra Mercosur 
-
+#Cargamos librerías
 library(DT)
 library(readxl)
 library(tidyverse)
@@ -15,17 +15,11 @@ library(ggflags)
 
 
 # Importamos datos --------------------------------------------------------
-
-
-
 #Fran
 mercosur_intra <- read_excel("econ-708/proyectos/Proyecto-Final-Datos/Bases/mercosur.xlsx")
 
-#Para importar Sofi RStudio
-#mercosur_intra <- read_excel("C:/Users/sofi1/OneDrive/Escritorio/Económicas/Segundo Cuatri 2022/Datos/Curso/econ-708/proyectos/proyecto_final/Bases/mercosur.xlsx")
 
-#Filtramos los países de Mercosur (tanto para Reporter como para Partner)
-
+#Filtramos los países de Mercosur (tanto para Reporter como para Partner), y dejamos exportaciones de más de 1M U$D
 mercosur_intra1 <- filter(mercosur_intra, Reporter %in% c("Argentina","Brazil", "Uruguay", "Paraguay", "Venezuela"))
 mercosur_intra2 <- filter(mercosur_intra1, Partner %in% c("Argentina","Brazil", "Uruguay", "Paraguay", "Venezuela"))
 mercosur_intra2 <- filter(mercosur_intra2, 'Trade Value (US$)' >= 1000000)
@@ -35,8 +29,7 @@ mercosur_intra2 <- mercosur_intra2 %>% relocate(Year, .after = last_col())
 
 View(mercosur_intra2)
 
-#Creamos los nodos mercosur y g7 (y eliminamos duplicados)
-
+#Creamos los nodos mercosur (y eliminamos duplicados)
 nodos_merc <- mercosur_intra2 %>% select(Reporter)                       
 nodos_merc <- nodos_merc [!duplicated(nodos_merc), ]
 
@@ -45,18 +38,13 @@ nodos_merc1 <- nodos_merc1 [!duplicated(nodos_merc1), ]
 colnames(nodos_merc1)[1] <-  "Reporter"
                                         
 
-# Creamos nodos para cada año para observar el valor exportado por período para cada país ---------------------------------------------
-
-
+# Creamos nodos para cada año para observar el valor exportado total por período para cada país ---------------------------------------------
 nodos_merc_2000 <- mutate(nodos_merc, value = 0)
 nodos_merc_2010 <- mutate(nodos_merc, value = 0)
 nodos_merc_2019 <- mutate(nodos_merc, value = 0)
 
 
-
-# Valor total de las exportaciones para cada pais en cada periodo ---------
-
- 
+# Valor total de las exportaciones para cada pais en cada periodo --------- 
 for (i in 1:5) {
   
   nodos_merc_2000$value[i] <- sum(mercosur_intra2[mercosur_intra2$Reporter == nodos_merc_2000$Reporter[i] & mercosur_intra2$Year == 2000, ]$`Trade Value (US$)`)  
@@ -103,8 +91,6 @@ intra_merc_2019 <- ggnetwork(intra_merc_2019)
 
 
 # Codigos de paises (ggflags) ---------------------------------------------
-
-
 intra_merc_2000$country<-countrycode(intra_merc_2000$name,"country.name","iso2c")
 intra_merc_2000$country <- intra_merc_2000$country %>% tolower()
 
